e6fae8f0561f9b38ea7a69f528aed09f83f42a7b
Joti <hasnahenajoti1414@gmail.com>
2026-05-18 22:12:24 +0600

    connected to mongodb database

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(cors());
+
+const uri = "mongodb+srv://mediqueue:Hmf0fadzhc39IkFL@cluster0.u7ckqg9.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db('mediqueuedb');
+    const tutorsCollection = db.collection('tutors');
+
+    
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
